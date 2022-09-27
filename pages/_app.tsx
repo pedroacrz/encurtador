0c87f2eac293425e3b5d50a683208d4b8511bbc1
pedroacrz <ti.pedrocruz@gmail.com>
2022-09-26 23:19:11 -0300

    chore: create home page and mock

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react"
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+  default: {
+    "primary": "#FE715D",
+    "secondary": "#F4724A",
+    "terciary": "#631C1C",
+    "background": "#080A0B",
+    "white": "#FFFFFF",
+    "gray": "#959595",
+    "zinc": "#202020",
+    "blue": "#2F2E41",
+  }
+}
+
+const theme = extendTheme({ colors })
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
 
 export default MyApp
