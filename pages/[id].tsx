290d002cde193fce7458b057f32070a97be018fd
pedroacrz <ti.pedrocruz@gmail.com>
2022-09-29 22:20:29 -0300

    perf: redirect

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -5,30 +5,42 @@ import { useEffect } from "react";
 import { useRouter } from 'next/router'
 import axios from "axios";
 
+const mysql = require("serverless-mysql")({
+    config: {
+        host: process.env.DB_HOST,
+        database: process.env.DB_DATABASE,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS
+    }
+})
 
 const Id: NextPage = () => {
     const router = useRouter()
     const { id } = router.query
 
-    useEffect(() => {
-        const getRedirection = async () => {
-            if (id) {
-                try {
-                    const response = await axios.get(`/api/getShortenedUrl/${id}`);
-                    let urlToRedirect: string = response.data.result?.url
-                    if (urlToRedirect.startsWith("http://") || urlToRedirect.startsWith("https://")) {
-                        window.location.replace(response.data.result?.url)
-                        return;
-                    }
-                    window.location.replace("https://" + response.data.result?.url)
-                } catch (error) {
-                    console.log(error)
-                }
-            }
-        }
+    // useEffect(() => {
 
-        getRedirection()
-    }, [id])
+    //     const getRedirection = async () => {
+    //         if (id) {
+    //             try {
+    //                 const response = await axios.get(`/api/getShortenedUrl/${id}`);
+    //                 let urlToRedirect: string = response.data.result?.url
+    //                 if (urlToRedirect.startsWith("http://") || urlToRedirect.startsWith("https://")) {
+    //                     //window.location.replace(response.data.result?.url)
+
+
+    //                     return;
+    //                 }
+
+    //                 //window.location.replace("https://" + response.data.result?.url)
+    //             } catch (error) {
+    //                 console.log(error)
+    //             }
+    //         }
+    //     }
+
+    //     getRedirection()
+    // }, [id])
 
     return (
         <div>
@@ -37,10 +49,32 @@ const Id: NextPage = () => {
                 <meta name="description" content="Redirecionando..." />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-
-            <Box background="default.white" height="100vh"></Box>
         </div >
     )
+}
+
+export async function getServerSideProps(context: any) {
+    const { res } = context;
+    console.time("a")
+    let results = await mysql.query(`select * from shortened_links where shortened_url = "${context.params.id}"`);
+    console.timeEnd("a")
+
+    let urlToRedirect: string = results[0]?.url
+    if (urlToRedirect.startsWith("http://") || urlToRedirect.startsWith("https://")) {
+        res.setHeader("location", urlToRedirect);
+        res.statusCode = 302;
+        res.end(); 
+        return {
+            props: {},
+        }
+    }
+    res.setHeader("location", "https://" + urlToRedirect);
+    res.statusCode = 302;
+    res.end(); 
+
+    return {
+        props: {},
+    }
 }
 
 export default Id
